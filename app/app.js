34a8eabee4f083c3e6ddb6046669dda82e416ded
LorenB <loren.barcus@gmail.com>
2014-10-01 18:11:22 -0400

    Added self-grading feature, which automatically removes takes cards out or circulation once they've answered correctly. Modified data structures, and ensure card data moves between structures withouth replication.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+var curCard;
+
 (function () {
 
   'use strict';
@@ -9,30 +11,86 @@
     console.log("test");
     $scope.currentCardNum = 0;
     $scope.isAnswerView = false;
+    $scope.activeCardsExist = true;
+    $scope.currentCard;
     
 
-    $scope.cards = [
-        {"question": "Do you like Angular?", "answer": "Yes"},
-        {"question": "Do you like JavaScript?", "answer": "No"}
+    $scope.activeCards = [
+        {"card_id":"a", "question": "Do you like Angular?", "answer": "Yes", "isCorrect": false},
+        {"card_id":"b", "question": "Do you like JavaScript?", "answer": "No", "isCorrect": false},
+        {"card_id":"b", "question": "Do you like python?", "answer": "Yes", "isCorrect": false}
     ]
-    $scope.getCard = function(cardNum) {
-      console.log("executing getCard with args: " + cardNum);
-      $scope.currentCardNum = cardNum;
-      console.log($scope.cards[cardNum]);
-      $scope.currentCard = $scope.cards[cardNum];
+    $scope.inactiveCards = [];
+/*
+    $scope.attempts = [{"card_id": "", "timestamp": "", "is_correct": ""}];
+
+    $scope.pefrormance = {"count": 0, "total_questions_answered": 0, "rounds": [{"round_id": "", "questions_answered": }] };
+    $scope.grading = {};
+*/
+
+    $scope.getCard = function() {
+      
+      /*
+      for (var i = 0; i < $scope.activeCards.length; i++) {
+        if($scope.activeCards[i]["isCorrect"]==false){
+          $scope.activeCards.push($scope.activeCards[i]);
+        }
+      };
+
+      var maximum = $scope.activeCards.length;
+      var minimum = 0;
+      var randomnumber = Math.floor(Math.random() * (maximum - minimum)) + minimum;
+      console.log(randomnumber);
+      
+      $scope.currentCard = $scope.activeCards[randomnumber];
+      console.log($scope.activeCards);
+
       // Anytime a card is changed display the quesion
       $scope.isAnswerView = false;
-
+      */
     }
+
+    $scope.setCurrentCard = function(){
+      console.log($scope.currentCard);
+      if($scope.activeCards.length > 0){
+        if(typeof $scope.currentCard != "undefined"){
+          console.log("currentCard exists.");
+          $scope.activeCards.unshift($scope.currentCard);
+        } else {
+          console.log("currentCard does not exists.");
+        }
+        $scope.currentCard = $scope.activeCards.pop();
+
+        $scope.isAnswerView = false;
+      } else {
+        $scope.activeCardsExist = false;
+      }
+      curCard = $scope.currentCard;
+    }
+
     $scope.showAnswer = function(){
       $scope.isAnswerView = true;
     }
     $scope.showQuestion = function(){
       $scope.isAnswerView = false;
     }
-    $scope.getCard(0);
-    console.log($scope.currentCard);
-    console.log($scope.cards[1])
+
+    $scope.gradeCurrentCard = function(isCorrect){
+      $scope.currentCard["isCorrect"] = isCorrect;
+      if(isCorrect == true){
+        $scope.inactiveCards.push($scope.currentCard);
+        $scope.currentCard = undefined;
+      } else {
+        $scope.activeCards.push($scope.currentCard);
+        $scope.currentCard = undefined;
+      }
+      console.log($scope.activeCards);
+      console.log($scope.inactiveCards);
+      $scope.setCurrentCard();
+
+    }
+
+    $scope.setCurrentCard();
 
   }]);
 
